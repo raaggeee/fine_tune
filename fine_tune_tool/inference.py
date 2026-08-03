7bf70ffa73d65fb87de58dc596f1633bbbfdf9ec
raaggeee <raaggee08@gmail.com>
2026-08-03 17:44:11 +0000

    debug

--- fine_tune_tool/inference.py
+++ fine_tune_tool/inference.py
@@ -3,7 +3,7 @@ from transformers import TextStreamer
 
 
 max_seq_length = 2048
-model_name = "google/gemma-4-E2B"
+model_name = "unsloth/gemma-4-E2B"
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name=model_name,
     max_seq_length=max_seq_length,
@@ -11,6 +11,9 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 )
 
 FastLanguageModel.for_inference(model)
+
+inputs = tokenizer("Hey!", return_tensors="pt").to("cuda")
+
 text_streamer = TextStreamer(tokenizer)
 
-print(model.generate("Hey!", streamer=text_streamer, max_new_tokens=100))
+print(model.generate(**inputs, streamer=text_streamer, max_new_tokens=100))
